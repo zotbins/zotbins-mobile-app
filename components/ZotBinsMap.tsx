33b3a7a513d8857e7c6d34850618f268f1e4c053
cyrusbuffington <cyrusbuffington@gmail.com>
2025-01-05 18:01:31 -0800

    added basic direction functionality

--- components/ZotBinsMap.tsx
+++ components/ZotBinsMap.tsx
@@ -1,22 +1,33 @@
 import BinStatusModal from "@/components/BinStatusModal";
 import Mapbox, {
   Camera,
+  LineLayer,
   LocationPuck,
   MapView,
   PointAnnotation,
+  ShapeSource,
 } from "@rnmapbox/maps";
 import React, { useRef, useState } from "react";
 import { Image, View } from "react-native";
 import ZotBinsLogo from "../assets/images/zotbins_logo.png";
 import { markers } from "../assets/markers.js";
 
+import mapboxSdk from '@mapbox/mapbox-sdk';
+import mapboxDirections from '@mapbox/mapbox-sdk/services/directions';
+
 Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOXACCESSTOKEN as string);
 Mapbox.setTelemetryEnabled(false);
+
+const directionsClient = mapboxDirections(mapboxSdk({ accessToken: process.env.EXPO_PUBLIC_MAPBOXACCESSTOKEN as string }));
+
 
 const ZotBinsMap = () => {
   const [displayModal, setDisplayModal] = useState(false);
   const [activeBinName, setActiveBinName] = useState("");
-
+  const [activeBinCoordinates, setActiveBinCoordinates] = useState<number[]>([]);
+  const [route, setRoute] = useState<any>(null);
+  const [userLocation, setUserLocation] = useState<number[]>([]);
+  
   // make type more specific instead of "any"
   const markerRefs: { [key: string]: any } = useRef({});
 
@@ -24,6 +35,34 @@ const ZotBinsMap = () => {
     setDisplayModal(false);
     setActiveBinName("");
   };
+
+  const getDirections = async (start: number[], end: number[]) => {
+    try {
+      const response = await directionsClient.getDirections({
+          profile: 'walking',
+          waypoints: [
+              { coordinates: [start[0], start[1]] },
+              { coordinates: [end[0], end[1]] }
+          ],
+          geometries: 'geojson'
+        }).send();
+
+        if (response && response.body && response.body.routes.length) {
+          setRoute(response.body.routes[0].geometry);
+        }
+    } 
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+  const activateRouting = () => {
+    // start is set to first bin for now, needs to be changed to user location
+    const start = [markers[0].longitude, markers[0].latitude];
+    const end = activeBinCoordinates;
+    getDirections(start, end);
+  }
+    
 
   return (
     <View className="w-full h-full">
@@ -59,6 +98,7 @@ const ZotBinsMap = () => {
             onSelected={() => {
               setDisplayModal(true);
               setActiveBinName(marker.name);
+              setActiveBinCoordinates([marker.longitude, marker.latitude]);
             }}
           >
             <Image
@@ -69,10 +109,28 @@ const ZotBinsMap = () => {
             />
           </PointAnnotation>
         ))}
+
+        {route && (
+          <ShapeSource id="routeSource" shape={route}>
+            <LineLayer
+              id="routeFill"
+              style={{
+                lineColor: "#66aec4",
+                lineWidth: 3,
+                lineCap: "round",
+                lineJoin: "round",
+              }}
+            />
+          </ShapeSource>
+        )}
       </MapView>
       {displayModal && (
         <View className="justify-center items-center">
-          <BinStatusModal name={activeBinName} closeModal={closeModal} />
+          <BinStatusModal 
+            name={activeBinName} 
+            closeModal={closeModal}
+            activateRouting={activateRouting}
+            />
         </View>
       )}
     </View>
