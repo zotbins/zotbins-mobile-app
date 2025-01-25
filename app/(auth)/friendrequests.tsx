0f5f17265ffbfc4575b97424d4595a155462c8f9
Evan Servito <78441979+evanpservito@users.noreply.github.com>
2025-01-24 16:18:21 -0800

    update loading

--- app/(auth)/friendrequests.tsx
+++ app/(auth)/friendrequests.tsx
@@ -186,125 +186,138 @@ const FriendRequests = () => {
 
   if (loading) {
     return (
-      <View>
-        <Text>Loading</Text>
+      <View className="pt-32 flex-1 p-5 bg-white">
+        <Stack.Screen
+          options={{
+            headerShadowVisible: false,
+            headerBackVisible: false,
+            headerTransparent: true,
+            headerLeft: () => <BackButton />,
+            headerTitle: "",
+          }}
+        />
+        <Text className="text-xl">Loading</Text>
+      </View>
+    );
+  } else {
+    return (
+      <View className="pt-32 flex-1 p-5 bg-white">
+        <Stack.Screen
+          options={{
+            headerShadowVisible: false,
+            headerBackVisible: false,
+            headerTransparent: true,
+            headerLeft: () => <BackButton />,
+            headerTitle: "",
+          }}
+        />
+        <View className="flex-row mb-5">
+          {/* sent requests tabs */}
+          <Pressable
+            className={`flex-1 p-3 items-center border-b-2 ${
+              activeTab === "sent" ? "border-blue-500" : "border-gray-300"
+            }`}
+            onPress={() => setActiveTab("sent")}
+          >
+            <Text className="text-lg font-bold text-gray-700">
+              Sent Requests
+            </Text>
+          </Pressable>
+
+          {/* received requests tab */}
+          <Pressable
+            className={`flex-1 p-3 items-center border-b-2 ${
+              activeTab === "received" ? "border-blue-500" : "border-gray-300"
+            }`}
+            onPress={() => setActiveTab("received")}
+          >
+            <Text className="text-lg font-bold text-gray-700">
+              Received Requests
+            </Text>
+          </Pressable>
+        </View>
+
+        {/* tab screen for sent requests*/}
+        {activeTab === "sent" ? (
+          <View>
+            <View className="flex-row items-center mb-5">
+              {/* search username in search box and add friend */}
+              <TextInput
+                className="flex-1 border border-gray-300 rounded px-3 py-2 mr-2"
+                placeholder="Enter username"
+                value={username}
+                onChangeText={setUsername}
+              />
+              <Pressable
+                className="bg-tealMed px-4 py-2 rounded"
+                onPress={handleAddFriend}
+              >
+                <Text className="text-white font-bold">Add Friend</Text>
+              </Pressable>
+            </View>
+
+            {/* view list of sent requests */}
+            <Text className="text-xl font-bold mb-3">
+              Friend Requests Sent:
+            </Text>
+            {friendRequestsSent.length > 0 ? (
+              friendRequestsSent.map((request: string, index: number) => (
+                <Text key={index} className="text-lg py-1">
+                  {request}
+                </Text>
+              ))
+            ) : (
+              <Text className="text-gray-500">No sent requests.</Text>
+            )}
+          </View>
+        ) : (
+          // tab screen for received requests
+
+          // If a request was sent successfully, the sender's username should be appended
+          // to the recipient's friendRequestsReceived array, and the recipient's username
+          // should be appended to the sender's friendRequestsSent array
+
+          <View>
+            <Text className="text-xl font-bold mb-3">
+              Friend Requests Received:
+            </Text>
+            {friendRequestsReceived!.length > 0 ? (
+              friendRequestsReceived!.map((request, index) => (
+                <View
+                  key={index}
+                  className="flex-row items-center justify-between mb-3"
+                >
+                  <Text className="text-lg">{request}</Text>
+                  <View className="flex-row">
+                    <Pressable
+                      className="bg-green-500 px-3 py-1 rounded mr-2"
+                      onPress={() => handleAccept(request)}
+                    >
+                      <Text className="text-white font-bold">Accept</Text>
+                    </Pressable>
+                    <Pressable
+                      className="bg-yellow-500 px-3 py-1 rounded mr-2"
+                      onPress={() => handleDecline(request)}
+                    >
+                      <Text className="text-white font-bold">Decline</Text>
+                    </Pressable>
+                    <Pressable
+                      className="bg-red px-3 py-1 rounded"
+                      onPress={() => handleBlock(request)}
+                    >
+                      <Text className="text-white font-bold">Block</Text>
+                    </Pressable>
+                  </View>
+                </View>
+              ))
+            ) : (
+              <Text className="text-gray-500">No received requests.</Text>
+            )}
+          </View>
+        )}
       </View>
     );
   }
-
-  return (
-    <View className="pt-32 flex-1 p-5 bg-white">
-      <Stack.Screen
-        options={{
-          headerShadowVisible: false,
-          headerBackVisible: false,
-          headerTransparent: true,
-          headerLeft: () => <BackButton />,
-          headerTitle: "",
-        }}
-      />
-      <View className="flex-row mb-5">
-        {/* sent requests tabs */}
-        <Pressable
-          className={`flex-1 p-3 items-center border-b-2 ${
-            activeTab === "sent" ? "border-blue-500" : "border-gray-300"
-          }`}
-          onPress={() => setActiveTab("sent")}
-        >
-          <Text className="text-lg font-bold text-gray-700">Sent Requests</Text>
-        </Pressable>
-
-        {/* received requests tab */}
-        <Pressable
-          className={`flex-1 p-3 items-center border-b-2 ${
-            activeTab === "received" ? "border-blue-500" : "border-gray-300"
-          }`}
-          onPress={() => setActiveTab("received")}
-        >
-          <Text className="text-lg font-bold text-gray-700">
-            Received Requests
-          </Text>
-        </Pressable>
-      </View>
-
-      {/* tab screen for sent requests*/}
-      {activeTab === "sent" ? (
-        <View>
-          <View className="flex-row items-center mb-5">
-            {/* search username in search box and add friend */}
-            <TextInput
-              className="flex-1 border border-gray-300 rounded px-3 py-2 mr-2"
-              placeholder="Enter username"
-              value={username}
-              onChangeText={setUsername}
-            />
-            <Pressable
-              className="bg-tealMed px-4 py-2 rounded"
-              onPress={handleAddFriend}
-            >
-              <Text className="text-white font-bold">Add Friend</Text>
-            </Pressable>
-          </View>
-
-          {/* view list of sent requests */}
-          <Text className="text-xl font-bold mb-3">Friend Requests Sent:</Text>
-          {friendRequestsSent.length > 0 ? (
-            friendRequestsSent.map((request: string, index: number) => (
-              <Text key={index} className="text-lg py-1">
-                {request}
-              </Text>
-            ))
-          ) : (
-            <Text className="text-gray-500">No sent requests.</Text>
-          )}
-        </View>
-      ) : (
-        // tab screen for received requests
-
-        // If a request was sent successfully, the sender's username should be appended
-        // to the recipient's friendRequestsReceived array, and the recipient's username
-        // should be appended to the sender's friendRequestsSent array
-
-        <View>
-          <Text className="text-xl font-bold mb-3">
-            Friend Requests Received:
-          </Text>
-          {friendRequestsReceived.length > 0 ? (
-            friendRequestsReceived.map((request, index) => (
-              <View
-                key={index}
-                className="flex-row items-center justify-between mb-3"
-              >
-                <Text className="text-lg">{request}</Text>
-                <View className="flex-row">
-                  <Pressable
-                    className="bg-green-500 px-3 py-1 rounded mr-2"
-                    onPress={() => handleAccept(request)}
-                  >
-                    <Text className="text-white font-bold">Accept</Text>
-                  </Pressable>
-                  <Pressable
-                    className="bg-yellow-500 px-3 py-1 rounded mr-2"
-                    onPress={() => handleDecline(request)}
-                  >
-                    <Text className="text-white font-bold">Decline</Text>
-                  </Pressable>
-                  <Pressable
-                    className="bg-red px-3 py-1 rounded"
-                    onPress={() => handleBlock(request)}
-                  >
-                    <Text className="text-white font-bold">Block</Text>
-                  </Pressable>
-                </View>
-              </View>
-            ))
-          ) : (
-            <Text className="text-gray-500">No received requests.</Text>
-          )}
-        </View>
-      )}
-    </View>
-  );
 };
 
 export default FriendRequests;
