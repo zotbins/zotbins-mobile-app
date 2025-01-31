dea592d096e21ac065c99b065a72975d6b40a144
sophiabtran <150560518+sophiabtran@users.noreply.github.com>
2025-01-30 16:36:21 -0800

    added tabs and friend scores, fixed profile page friend list not showing

--- app/(auth)/leaderboard.tsx
+++ app/(auth)/leaderboard.tsx
@@ -2,7 +2,8 @@ import BackButton from "@/components/Reusables/BackButton";
 import firestore from "@react-native-firebase/firestore";
 import { Stack, useRouter } from "expo-router";
 import React, { useEffect, useState, useRef } from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import { Image, ScrollView, Text, View, TouchableOpacity } from "react-native";
 import { currentUserUid } from "../_layout";
 import auth from "@react-native-firebase/auth";
 import storage from "@react-native-firebase/storage";
@@ -13,14 +14,17 @@ interface LeaderboardUser {
   points: number;
   username: string;
 }
-
+const trophy = require('../../assets/images/trophy.png');
 const Leaderboard = () => {
   const router = useRouter();
-
-  const [leaderboardData, setLeaderboardData] = useState<LeaderboardUser[]>([]);
+  const [allLeaderboardData, setAllLeaderboardData] = useState<LeaderboardUser[]>([]);
+  const [weeklyLeaderboardData, setWeeklyLeaderboardData] = useState<LeaderboardUser[]>([]);
+  const [friendLeaderboardData, setFriendLeaderboardData] = useState<LeaderboardUser[]>([]);
   const [userScore, setUserScore] = useState<number>(0);
   const [userRank, setUserRank] = useState<number>(0);
-
+  const [username, setUsername] = useState<string>("");
+  const [activeTab, setActiveTab] = useState('all-rankings');
+  const [friendList, setFriendList] = useState<string[]>([]);
   const getProfilePicUrl = async (uid: string) => {
     try {
       const imageRef = storage().ref(`zotzero-user-profile-pics/${uid}`);
@@ -33,7 +37,8 @@ const Leaderboard = () => {
 
   /**
    * @description Gets descending order of scores and searches to find the current user's score.
-   * Adds rank, usernames, and scores to the leaderboard. Leaderboard only displays top 10 users.
+   * Adds profile pictures, rank, usernames, and scores to the leaderboard. Leaderboard only displays 
+   * top 10 users unless friends are being displayed. 
    */
   const dataLoaded = useRef(false);
   useEffect(() => {
@@ -46,23 +51,25 @@ const Leaderboard = () => {
 
         const querySnapshot = await leaderboardQuery.get();
         const leaderboard: LeaderboardUser[] = [];
+
         for (let doc of querySnapshot.docs) {
-          let name = doc.data().username;
           if (doc.data().uid == currentUserUid) {
+            setUsername(doc.data().username);
             setUserScore(doc.data().totalPoints);
             setUserRank(leaderboard.length + 1);
+            setFriendList(doc.data().friendsList || []);
           }
-          
           const pfpUrl = await getProfilePicUrl(doc.data().uid);
           leaderboard.push({
             pfp: pfpUrl,
             rank: leaderboard.length + 1,
-            username: name,
+            username: doc.data().username,
             points: doc.data().totalPoints,
           });
         }
+        setAllLeaderboardData(leaderboard.slice(0, 10));
+        setWeeklyLeaderboardData(leaderboard.slice(0, 10));
         dataLoaded.current = true;
-        setLeaderboardData(leaderboard.slice(0, 10));
       } catch (error) {
         console.error("Error fetching leaderboard data: ", error);
       }
@@ -71,12 +78,64 @@ const Leaderboard = () => {
     fetchLeaderboard();
   }, []);
 
-  const currentUser = { rank: userRank, username: "You", points: userScore };
+  const renderLeaderboard = () => {
+    switch (activeTab) {
+      case 'weekly':
+        return weeklyLeaderboardData.map((user, index) => renderLeaderboardRow(user, index));
+      case 'friends':
+        return friendLeaderboardData.map((user, index) => renderLeaderboardRow(user, index));
+      case 'all-rankings':
+      default:
+        return allLeaderboardData.map((user, index) => renderLeaderboardRow(user, index));
+    }
+  };
+
+
+  /**
+   * @description Takes the entire leaderboard and filters for friends.
+   */
+  useEffect(() => {
+    if (friendList.length > 0 && dataLoaded) {
+      const filteredLeaderboard = allLeaderboardData.filter((user) =>
+        friendList.includes(user.username) || (user.username === username)
+      );
+
+      const rankedLeaderboard = filteredLeaderboard.map((user, index) => ({
+        ...user,
+        rank: index + 1,
+      }));
+
+      setFriendLeaderboardData(rankedLeaderboard);
+    }
+  }, [friendList, dataLoaded, allLeaderboardData]);
+
+  // Helper function to render leaderboard rows
+  const renderLeaderboardRow = (user: LeaderboardUser, index: number) => (
+    <React.Fragment key={user.rank}>
+      <View className="flex flex-row items-center mb-2 w-full px-4">
+        <Text className="text-xl text-black w-1/4 pl-4">{user.rank}</Text>
+        <View className="w-1/2 flex flex-row items-center justify-start">
+          <Image
+            source={{ uri: user.pfp }}
+            className={`w-10 h-10 rounded-full mr-4 ${user.username === username ? 'border-2 border-tintColor' : 'border-2 border-gray-200'}`}
+          />
+          <Text className={`w-full text-xl ${user.username === username ? 'text-tintColor' : 'text-black'}`}>
+            {user.username === username ? 'You' : user.username}
+          </Text>
+        </View>
+        <Text className="text-xl w-1/4 text-center text-black">{user.points}</Text>
+      </View>
+        <View className="w-full mb-4 w-full px-4 border-b border-gray-300 py-2" />
+    </React.Fragment>
+  );
 
   return (
-    <ScrollView
+    <SafeAreaProvider >
+      <SafeAreaView className="bg-white flex-1">
+      <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
-      className="pt-24 bg-white"
+      className="bg-white"
+      automaticallyAdjustsScrollIndicatorInsets={true}
     >
       <Stack.Screen
         options={{
@@ -88,54 +147,48 @@ const Leaderboard = () => {
         }}
       />
       <View className="flex flex-col items-center px-8">
-        {/* <Text className="text-xl text-black font-semibold py-4 mb-6 w-full text-center">
-          Your Rank: {currentUser.rank} with {currentUser.points} Points
-        </Text> */}
-        
-        <Text className="text-3xl font-medium text-black mb-6">All Rankings</Text>
-        {/* Renders top 10 on leaderboard, if user is in top 10 then bolds user */}
+      <Image
+        source={trophy}
+        style={{
+          width: 100,
+          height: 100,
+        }}
+      />
+      <Text className="text-4xl text-black font-semibold my-2">Leaderboard</Text>
+
+        <View className="flex flex-row mb-6 mt-2">
+          <TouchableOpacity 
+            onPress={() => setActiveTab('all-rankings')}
+            className={`mr-6 ${activeTab === 'all-rankings' ? 'font-semibold border-b-2 border-tintColor' : 'text-black'}`}
+          >
+            <Text className={`text-xl ${activeTab === 'all-rankings' ? 'text-darkTintColor' : 'text-black'}`}>All</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            onPress={() => setActiveTab('weekly')}
+            className={`mr-6 ${activeTab === 'weekly' ? 'font-semibold border-b-2 border-tintColor' : 'text-black'}`}
+          >
+            <Text className={`text-xl ${activeTab === 'weekly' ? 'text-darkTintColor' : 'text-black'}`}>Weekly</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            onPress={() => setActiveTab('friends')}
+            className={`${activeTab === 'friends' ? 'font-semibold border-b-2 border-tintColor' : 'text-black'}`}
+          >
+            <Text className={`text-xl ${activeTab === 'friends' ? 'text-darkTintColor' : 'text-black'}`}>Friends</Text>
+          </TouchableOpacity>
+        </View>
+
         <View className="flex flex-row items-center mb-4 w-full px-4 border-b border-gray-300 py-2">
           <Text className="text-xl text-black font-semibold w-1/4 text-left">Rank</Text>
           <Text className="text-xl text-black font-semibold w-1/2 text-left pl-14">User</Text>
           <Text className="text-xl text-black font-semibold w-1/4 text-right pr-3">Points</Text>
         </View>
-        {leaderboardData.map((user, index) =>
-          <React.Fragment key={user.rank}>
-          <View className="flex flex-row items-center mb-2 w-full px-4">
-            <Text className="text-xl text-black font-semibold w-1/4 pl-4">
-              {user.rank}
-            </Text>
-      
-            <View className="w-1/2 flex flex-row items-center justify-start">
-              <Image
-                source={{ uri: user.pfp }}
-                className={`w-10 h-10 rounded-full mr-4 ${
-                  user.rank === userRank ? 'border-2 border-tintColor' : 'border-2 border-gray-200'
-                }`}
-              />
-              <Text
-                className={`text-xl ${
-                  user.rank === userRank ? 'text-tintColor' : 'text-black'
-                }`}
-              >
-                {user.rank === userRank ? 'You' : user.username}
-              </Text>
-            </View>
-      
-            <Text
-              className="text-xl w-1/4 text-center text-black">
-              {user.points}
-            </Text>
-          </View>
-      
-          {index < leaderboardData.length - 1 && (
-            <View className="w-full mb-4 w-full px-4 border-b border-gray-300 py-2" />
-          )}
-        </React.Fragment>
-        )}
+
+        {renderLeaderboard()}
       </View>
     </ScrollView>
-  );  
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 };
 
 export default Leaderboard;
