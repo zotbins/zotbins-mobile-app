da6a6c6428ce4bca76b6e6ebe648e30c97d90704
cyrusbuffington <cyrusbuffington@gmail.com>
2025-02-27 09:56:24 -0800

    set up daily mission cloud fxn (not tested)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,7 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+const admin = require("firebase-admin");
+const updateDailyMissions = require("./updateDailyMissions");
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+admin.initializeApp();
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.updateDailyMissions = updateDailyMissions;
---
+++ functions/src/updateDailyMissions.ts
@@ -0,0 +1,26 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+exports.updateDailyMissions = functions.pubsub.schedule("every day 00:00").onRun(async () => {
+    const db = admin.firestore();
+    // get all missions with type "daily"
+    const missions = await db.collection("missions").where("type", "==", "daily").get();
+    const batch = db.batch();
+    // set all missions to have status false
+    missions.forEach((mission: any) => {
+        batch.update(mission.ref, { status: false });
+    });
+
+    // choose 3 random daily missions to make active
+    const dailyMissions = missions.docs.map((mission: any) => mission.data());
+    const activeMissions = dailyMissions.sort(() => 0.5 - Math.random()).slice(0, 3);
+    activeMissions.forEach((mission: any)  => {
+        const missionRef = db.collection("missions").doc(mission.id);
+        batch.update(missionRef, { status: true });
+    });
+    await batch.commit();
+
+    console.log("Daily missions updated");
+    return null;
+}
+);
