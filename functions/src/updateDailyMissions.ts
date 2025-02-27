163fca4ff107f2f2e091192953d675787dbd7446
cyrusbuffington <cyrusbuffington@gmail.com>
2025-02-27 12:06:55 -0800

    updateDailyMission cloud fxn tested and working

--- functions/src/updateDailyMissions.ts
+++ functions/src/updateDailyMissions.ts
@@ -3,29 +3,35 @@ import * as admin from 'firebase-admin';
 
 const updateDailyMissionsLogic = async () => {
     const db = admin.firestore();
-    // get all missions with type "daily" and status is active
-    const missions = await db.collection("missions").where("type", "==", "daily").where("status", "==", true).get();
-    const batch = db.batch();
-    // set all missions to have status false
-    missions.forEach((mission: any) => {
-        batch.update(mission.ref, { status: false });
-    });
+    try {
+        // get all missions with type "daily"
+        const missions = await db.collection("missions").where("type", "==", "daily").get();
 
-    const updatedDocIds: string[] = [];
-    // choose 3 random daily missions to make active
-    const dailyMissions = missions.docs.map((mission: any) => mission.data());
-    const activeMissions = dailyMissions.sort(() => 0.5 - Math.random()).slice(0, 3);
-    activeMissions.forEach((mission: any) => {
-        const missionRef = db.collection("missions").doc(mission.id);
-        updatedDocIds.push(mission.id);
-        batch.update(missionRef, { status: true });
-    });
-    await batch.commit();
+        if (missions.empty) {
+            console.log("No daily missions found.");
+            return;
+        }
 
-    console.log("Daily missions updated");
-    console.log("Updated missions IDs: ", updatedDocIds);
+        const batch = db.batch();
+        // set all missions to have status false
+        missions.forEach((mission: any) => {
+            batch.update(mission.ref, { status: false });
+        });
+
+        // choose 3 random daily missions to make active
+        const randomMissions = missions.docs.sort(() => Math.random() - 0.5).slice(0, 3);
+        randomMissions.forEach((mission: any) => {
+            batch.update(mission.ref, { status: true });
+        });
+        await batch.commit();
+        
+        console.log("Daily missions updated");
+    } catch (error) {
+        console.error("Error updating daily missions: ", error);
+    }
     return null;
 };
+
 
 export const updateDailyMissions = functions.pubsub.schedule("every day 00:00").onRun(updateDailyMissionsLogic);
 
