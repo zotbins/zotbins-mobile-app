23b941dca92f1014f8710dcb89fdcbf9778f0ecf
heartybp <parrenashearty@gmail.com>
2025-04-11 03:55:59 -0700

    styling progress for achievements

--- app/(auth)/achievements.tsx
+++ app/(auth)/achievements.tsx
@@ -1,101 +1,114 @@
 import BackButton from "@/components/Reusables/BackButton";
 import { Stack } from "expo-router";
 import React, { useEffect, useState } from "react";
-import { View, Text} from "react-native";
+import { View, Text, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { getFirestore, doc, FieldValue, collection, getDocs, } from "@react-native-firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  FieldValue,
+  collection,
+  getDocs,
+} from "@react-native-firebase/firestore";
 import { getAuth, FirebaseAuthTypes } from "@react-native-firebase/auth";
+import AchievementIcon from "@/components/Profile/achievementImagePlaceholder.png";
 
 interface Achievement {
-    id: number;
-    name: string;
-    description: string;
-    reward: string;
-    actionAmount: number;
-    progress: number;
+  id: number;
+  name: string;
+  description: string;
+  reward: string;
+  actionAmount: number;
+  progress: number;
 }
 
-
 const Achievements = () => {
-    const [achievements, setAchievements] = useState<Achievement[]>([]);
-    const user = getAuth().currentUser;
-    useEffect(() => {
-        if (user) {
-            const fetchAchievements = async (user: FirebaseAuthTypes.User) => {
-                try {
-                    const db = getFirestore();
-                    const userRef = doc(db, "users", user.uid);
-                    const achievementsRef = collection(userRef, "achievements");
-                    const querySnapshot = await getDocs(achievementsRef);                    
-                    const achievementsData: Achievement[] = querySnapshot.docs.map((document) => {
-                        const data = document.data();
-                        return {
-                            id: data.id,
-                            name: data.name,
-                            description: data.description,
-                            reward: data.reward,
-                            actionAmount: data.actionAmount,
-                            progress: data.progress,
-                        };
-                    });
-
-                    setAchievements(achievementsData);
-
-                } catch (error) {
-                    console.error("Error fetching achievements:", error);
-                }
+  const [achievements, setAchievements] = useState<Achievement[]>([]);
+  const user = getAuth().currentUser;
+  useEffect(() => {
+    if (user) {
+      const fetchAchievements = async (user: FirebaseAuthTypes.User) => {
+        try {
+          const db = getFirestore();
+          const userRef = doc(db, "users", user.uid);
+          const achievementsRef = collection(userRef, "achievements");
+          const querySnapshot = await getDocs(achievementsRef);
+          const achievementsData: Achievement[] = querySnapshot.docs.map(
+            (document) => {
+              const data = document.data();
+              return {
+                id: data.id,
+                name: data.name,
+                description: data.description,
+                reward: data.reward,
+                actionAmount: data.actionAmount,
+                progress: data.progress,
+              };
             }
-            fetchAchievements(user);
-        } else {
-            console.error("User is not logged in");
+          );
+
+          setAchievements(achievementsData);
+        } catch (error) {
+          console.error("Error fetching achievements:", error);
         }
-    }, []);
+      };
+      fetchAchievements(user);
+    } else {
+      console.error("User is not logged in");
+    }
+  }, []);
 
-    return (
-        <SafeAreaView className="flex-1 bg-white">
-            <Stack.Screen
-                options={{
-                    headerShadowVisible: false,
-                    headerBackVisible: false,
-                    headerTransparent: true,
-                    headerLeft: () => <BackButton />,
-                    headerTitle: "",
-                }}
-            />
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Stack.Screen
+        options={{
+          headerShadowVisible: false,
+          headerBackVisible: false,
+          headerTransparent: true,
+          headerLeft: () => <BackButton />,
+          headerTitle: "",
+        }}
+      />
 
-            <View className="flex-1 px-4">
-                <Text className="text-2xl font-bold text-gray-800 mb-4 text-center">Achievements</Text>
+      <View className="flex-1 px-4 w-full">
+        {/* Achievements List */}
+        {achievements.map((achievement) => {
+          const fraction = `${achievement.progress}/${achievement.actionAmount}`;
+          const progressPercent =
+            (achievement.progress / achievement.actionAmount) * 100;
 
-                {/* Achievements List */}
-                {achievements.map((achievement) => {
-                    const fraction = `${achievement.progress}/${achievement.actionAmount}`;
-                    const progressPercent = (achievement.progress / achievement.actionAmount) * 100;
-
-                    return (
-                        <View
-                            key={achievement.id}
-                            className="bg-gray-100 p-4 rounded-lg mb-3 border border-gray-300"
-                        >
-                            <Text className="text-lg font-bold text-gray-700">{achievement.name}</Text>
-                            <Text className="text-gray-600">{achievement.reward}</Text>
-                            <View className="mt-3">
-                                <View className="relative w-full h-5 bg-gray-700 rounded">
-                                    {/* Use inline style only for the dynamic width */}
-                                    <View
-                                        className="absolute left-0 top-0 h-5 bg-green-500 rounded"
-                                        style={{ width: `${progressPercent}%` }}
-                                    />
-                                    <Text className="absolute w-full text-center text-white font-bold">
-                                        {fraction}
-                                    </Text>
-                                </View>
-                            </View>
-                        </View>
-                    );
-                })}
+          return (
+            <View
+              key={achievement.id}
+              className="bg-lightBackground p-4 rounded-[35] mb-3 border border-gray-300 flex flex-row"
+            >
+              <View>
+                <Image source={AchievementIcon} alt="" />
+              </View>
+              <View>
+                <Text className="text-lg font-bold text-gray-700">
+                  {achievement.name}
+                </Text>
+                <Text className="text-gray-600">{achievement.reward}</Text>
+                <View className="mt-3">
+                  <View className="relative w-full h-5 bg-gray-700 rounded">
+                    {/* Use inline style only for the dynamic width */}
+                    <View
+                      className="absolute left-0 top-0 h-5 bg-green-500 rounded"
+                      style={{ width: `${progressPercent}%` }}
+                    />
+                    <Text className="absolute w-full text-center text-white font-bold">
+                      {fraction}
+                    </Text>
+                  </View>
+                </View>
+              </View>
             </View>
-        </SafeAreaView>
-    );
+          );
+        })}
+      </View>
+    </View>
+  );
 };
 
 export default Achievements;
---
+++ app/(auth)/settings.tsx
@@ -0,0 +1,115 @@
+import { getAuth } from "@react-native-firebase/auth";
+import { router, useRouter } from "expo-router";
+import React, { useEffect, useState } from "react";
+import { Pressable, SafeAreaView, View, Text, Alert } from "react-native";
+
+import { getFirestore, doc, getDoc } from "@react-native-firebase/firestore";
+
+const Settings = () => {
+  const router = useRouter();
+
+  const user = getAuth().currentUser;
+
+  const [userDoc, setUserDoc] = useState<any>(null);
+  const [showPasswordForm, setShowPasswordForm] = useState(false);
+
+  // on user change, fetch user document from firestore
+  useEffect(() => {
+    const fetchUserDoc = async () => {
+      const uid = user?.uid;
+      if (!uid) {
+        return;
+      }
+
+      try {
+        const db = getFirestore();
+        const userDocRef = doc(db, "users", uid);
+        const userDocSnap = await getDoc(userDocRef);
+        if (!userDocSnap.exists) {
+          throw new Error("User document does not exist");
+        }
+        setUserDoc(userDocSnap.data());
+      } catch (error) {
+        console.error("Error fetching user document: ", error);
+        Alert.alert("Error", "Failed to fetch user document");
+      }
+    };
+
+    fetchUserDoc();
+  });
+
+  return (
+    <>
+      <SafeAreaView>
+        <Pressable
+          onPress={() => router.push("/envimpact")}
+          className="bg-blue py-3 rounded-lg"
+        >
+          <Text className="text-white text-center">
+            Check Environmental Impact
+          </Text>
+        </Pressable>
+
+        {/* Friends container */}
+        <View className="pb-2">
+          <Pressable
+            onPress={() => router.push("/friendrequests")}
+            className="bg-blue px-4 py-3 rounded-lg my-2 active:opacity-50"
+          >
+            <Text className="text-white text-center">Friend Requests</Text>
+          </Pressable>
+          <Text className="text-xl text-center font-bold">Friends</Text>
+          {userDoc?.friendsList?.length > 0 ? (
+            // TODO: Update types
+            userDoc.friendsList.map((friend: any, index: number) => (
+              <Text key={index} className="text-center">
+                {friend}
+              </Text>
+            ))
+          ) : (
+            <Text className="text-center">No friends added yet!</Text>
+          )}
+        </View>
+
+        <Pressable
+          onPress={() => router.push("/achievements")}
+          className="bg-blue px-4 py-3 rounded-lg my-2 active:opacity-50"
+        >
+          <Text className="text-white text-center">Achievements</Text>
+        </Pressable>
+
+        <Pressable
+          onPress={() => router.push("/faq")}
+          className="bg-blue px-4 py-3 rounded-lg my-2 active:opacity-50"
+        >
+          <Text className="text-white text-center">FAQ</Text>
+        </Pressable>
+
+        <Pressable
+          onPress={() => router.replace("/(auth)/spirittrash")}
+          className="bg-blue px-4 py-3 rounded-lg my-2 active:opacity-50"
+        >
+          <Text className="text-white text-center">
+            Retake Spirit Trash Quiz
+          </Text>
+        </Pressable>
+
+        <Pressable
+          onPress={() => getAuth().signOut()}
+          className="bg-blue px-4 py-3 rounded-lg my-2 active:opacity-50"
+        >
+          <Text className="text-white text-center">Sign Out</Text>
+        </Pressable>
+
+        <Pressable
+          onPress={() => router.push("/passwordchange")}
+          className="bg-blue px-4 py-3 rounded-lg my-2 active:opacity-50"
+        >
+          <Text className="text-white text-center">Change Password</Text>
+        </Pressable>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default Settings;
