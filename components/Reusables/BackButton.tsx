3f15ae22b2a0fe0707e5c45651c6930d88b81e02
EthanSantos <ethanangelo61@gmail.com>
2025-04-17 19:16:55 -0700

    new back button

--- components/Reusables/BackButton.tsx
+++ components/Reusables/BackButton.tsx
@@ -1,18 +1,16 @@
-import { Ionicons } from "@expo/vector-icons";
-import { router } from "expo-router";
 import React from "react";
 import { Pressable } from "react-native";
-import { Colors } from "react-native/Libraries/NewAppScreen";
+import { router } from "expo-router";
+import BackIcon from "@/assets/icons/BackIcon.svg";
 
-const BackButton = () => {
-  return (
-    <Pressable
-      className="bg-white rounded-full p-2 active:opacity-50"
-      onPress={() => router.back()}
-    >
-      <Ionicons name="chevron-back" size={24} color={Colors.black} />
-    </Pressable>
-  );
-};
+const BackButton = () => (
+  <Pressable
+    onPress={() => router.back()}
+    className="m-2 active:opacity-50"
+    style={{ width: 48, height: 48 }}
+  >
+    <BackIcon width={48} height={48} />
+  </Pressable>
+);
 
 export default BackButton;
