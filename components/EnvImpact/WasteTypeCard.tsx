8b3921a694522acdaab3bff727a9b0597345dac5
cyrusbuffington <cyrusbuffington@gmail.com>
2025-04-22 22:28:56 -0700

    changed icons on envimpact

--- components/EnvImpact/WasteTypeCard.tsx
+++ components/EnvImpact/WasteTypeCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text } from "react-native";
-import Ionicons from "react-native-vector-icons/Ionicons";
+import { FontAwesome5, Feather, Ionicons } from "@expo/vector-icons";
 
 interface ItemTypeCardProps {
   count: number;
@@ -9,16 +9,19 @@ interface ItemTypeCardProps {
 
 const WasteTypeCard: React.FC<ItemTypeCardProps> = ({ count, type }) => {
   // Define icon based on the type
+  let iconType = "ionicons";
   let icon = "";
   let label = "";
   
   switch (type.toLowerCase()) {
     case "landfill":
-      icon = "trash-outline";
+      iconType = "feather";
+      icon = "trash";
       label = "Discarded";
       break;
     case "recycled":
-      icon = "refresh";
+      iconType = "fontawesome5";
+      icon = "recycle";
       label = "Recycled";
       break;
     case "composted":
@@ -38,12 +41,17 @@ const WasteTypeCard: React.FC<ItemTypeCardProps> = ({ count, type }) => {
         {label}
       </Text>
       <View className="bg-white rounded-xl p-4 flex-row items-center justify-center">
-        <Ionicons name={icon} size={30} color="#48BB78" className="mr-2" />
+        {iconType === "ionicons" ? (
+          <Ionicons name={icon} size={30} color="#48BB78" className="mr-2" />
+        ) : iconType === "fontawesome5" ? (
+          <FontAwesome5 name={icon} size={24} color="#48BB78" className="mr-2" />
+        ) : (
+          <Feather name={icon} size={24} color="#48BB78" className="mr-2" />
+        )}
         <Text className="text-3xl text-center font-bold text-primaryGreen">
           {count}
         </Text>
       </View>
-
     </View>
   );
 };
