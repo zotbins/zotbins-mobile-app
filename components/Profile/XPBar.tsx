efec13376bf72f169735d904916e0a9550777865
Ryan Bui <buiswrld@gmail.com>
2025-04-23 21:05:31 -0700

    create separate XP bar component

---
+++ components/Profile/XPBar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { View } from "react-native";
+
+const XPBar = ({ xp, requiredXP }: { xp: number; requiredXP: number }) => {
+    const progress = Math.min(xp / requiredXP, 1) * 100;
+  
+    return (
+      <View className="w-[90%] h-3 bg-white border border-mediumGreen rounded-full overflow-hidden shadow-sm">
+        <View
+          style={{ width: `${progress}%` }}
+          className="h-full bg-primaryGreen rounded-full"
+        />
+      </View>
+    );
+  };
+  
