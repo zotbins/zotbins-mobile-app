1ecfe14c3c8b46d4e3267e55b12f016b96a3d8fd
Ryan Bui <buiswrld@gmail.com>
2025-04-23 21:45:58 -0700

    opt for percentage-based xpbar height and adjust pixel edge scaling

--- components/Profile/XPBar.tsx
+++ components/Profile/XPBar.tsx
@@ -6,7 +6,7 @@ const XPBar = ({ xp, requiredXP }: { xp: number; requiredXP: number }) => {
   const progress = Math.min(xp / requiredXP, 1) * 100;
 
   return (
-    <View className="w-[110px] h-[8px] bg-white border-[0.8px] border-mediumGreen rounded-full overflow-hidden">
+    <View className="w-[60%] h-[8px] bg-white border-[0.8px] border-mediumGreen rounded-full overflow-hidden">
       {/* main green filling part */}
       <View
         style={{ width: `${progress}%` }}
@@ -20,7 +20,7 @@ const XPBar = ({ xp, requiredXP }: { xp: number; requiredXP: number }) => {
             position: "absolute",
             left: `${progress}%`,
             top: 0,
-            transform: [{scaleY: 1.1}, { translateX: -1.5 }],
+            transform: [{scaleY: 1.5}, { translateX: -1.5 }],
             zIndex: 10,
             height: 12,
             width: 6,
