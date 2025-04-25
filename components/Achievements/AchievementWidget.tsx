68fabee2333266a622114d3d8f1ad1235169934f
EthanSantos <ethanangelo61@gmail.com>
2025-04-24 20:33:51 -0700

    achievement widget

---
+++ components/Achievements/AchievementWidget.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+import AchievementIcon from "@/components/Profile/achievementImagePlaceholder.png";
+import LinearGradient from "react-native-linear-gradient";
+
+interface AchievementProps {
+    achievement: {
+        id: number;
+        name: string;
+        description: string;
+        reward: string;
+        actionAmount: number;
+        progress: number;
+    };
+}
+
+const Achievement = ({ achievement }: AchievementProps) => {
+    const fraction = `${achievement.progress}/${achievement.actionAmount}`;
+    const progressPercent = (achievement.progress / achievement.actionAmount) * 100;
+
+    return (
+        <View className="mb-3 shadow-sm">
+            {/* Gradient Border Container */}
+            <LinearGradient
+                colors={["#018029", "#DFFFE3", "#b4fabd", "#004c18"]}
+                style={{
+                    padding: 1.3,
+                    borderRadius: 35,
+                }}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 1 }}
+                className="mb-4 shadow-lg"
+                locations={[0.1, 0.5, 0.8, 1]}
+            >
+                {/* Achievement Content */}
+                <View className="bg-lightBackground p-4 rounded-[33] flex flex-row items-center">
+                    <View>
+                        <Image source={AchievementIcon} alt="" />
+                    </View>
+                    <View className="ml-4 flex-1">
+                        <Text className="text-xl font-semibold text-darkestGreen">
+                            {achievement.name}
+                        </Text>
+                        <Text className="text-darkGreen">{achievement.reward}</Text>
+                        <View className="mt-3">
+                            <View className="relative w-full h-5 bg-darkGreen rounded-full">
+                                {/* Use inline style only for the dynamic width */}
+                                <View
+                                    className="absolute left-0 top-0 h-5 bg-green-500 rounded-full"
+                                    style={{ width: `${progressPercent}%` }}
+                                />
+                                <Text className="absolute w-full text-center text-white font-bold">
+                                    {fraction}
+                                </Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+            </LinearGradient>
+        </View>
+    );
+};
+
+export default Achievement;
