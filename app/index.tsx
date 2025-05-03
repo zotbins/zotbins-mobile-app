fd207bfe4163faadee55026aed42d22793ae41be
sophiabtran <transb1@uci.edu>
2025-05-03 15:15:08 -0700

    adjusted splash screen time and changed routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,56 @@
 import { View, Text, Image, Pressable } from "react-native";
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Colors from "@/constants/Colors";
 import { Redirect, useRouter } from "expo-router";
 import ZotbinsLogo from "../assets/images/zotbins_logo.png";
 import "../global.css";
 import { currentUser } from "./_layout";
+import * as SplashScreen from 'expo-splash-screen';
+import SplashScreenImage from "../assets/images/splash-screen.png";
 
+SplashScreen.preventAutoHideAsync();
 const Home = () => {
+  const [appIsReady, setAppIsReady] = useState(false);
   const router = useRouter();
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
 
-  const goToOnboarding = () => {
-    router.push("/onboarding");
-  };
+    prepare();
+  }, []);
+  
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
 
-  const goToLogin = () => {
-    router.push("/login");
-  };
-  if (!currentUser) {
-    return <Redirect href="/login" />;
-  } else {
-      return <Redirect href="/(auth)/(tabs)/home" />;
-  }
+  useEffect(() => {
+
+    if (appIsReady) {
+      if (!currentUser) {
+        router.push("/login");
+      } else {
+        router.push("/(auth)/(tabs)/home");
+      }
+    }
+  }, [appIsReady, currentUser, router]);
+
+  return (
+    <View onLayout={onLayoutRootView}>
+      <Image 
+        source={SplashScreenImage} 
+        resizeMode="contain"
+      />
+    </View>
+  );
 };
 
 export default Home;
