5ac9613e0908d8e4a41601fa3a49fab5eedae7c3
cyrusbuffington <cyrusbuffington@gmail.com>
2025-05-08 15:22:48 -0700

    update reanimated

--- components/Reusables/AccordionItem.tsx
+++ components/Reusables/AccordionItem.tsx
@@ -1,13 +1,7 @@
 import { Entypo } from "@expo/vector-icons";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, TouchableOpacity, Text, Pressable } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import Animated, { 
-  useSharedValue, 
-  useAnimatedStyle, 
-  withTiming, 
-  Easing 
-} from 'react-native-reanimated';
 
 interface AccordionItemProps {
   question: string;
@@ -21,24 +15,6 @@ const AccordionItem: React.FC<AccordionItemProps> = ({
   row,
 }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  
-  // Create a shared value for rotation angle
-  const rotateValue = useSharedValue(0);
-  
-  // Update rotation value when isOpen changes
-  useEffect(() => {
-    rotateValue.value = withTiming(isOpen ? 90 : 0, {
-      duration: 300,
-      easing: Easing.bezier(0.25, 0.1, 0.25, 1),
-    });
-  }, [isOpen]);
-  
-  // Create animated style for rotation
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      transform: [{ rotate: `${rotateValue.value}deg` }],
-    };
-  });
   
   return (
     <LinearGradient
@@ -54,9 +30,9 @@ const AccordionItem: React.FC<AccordionItemProps> = ({
         className="bg-lightBackground rounded-xl p-4"
       >
         <View className="flex flex-row items-center ">
-          <Animated.View style={animatedStyle}>
-            <Entypo name="chevron-right" size={24} color="#00762B" />
-          </Animated.View>
+          <View>
+            <Entypo name={isOpen ? "chevron-down" : "chevron-right"} size={24} color="#00762B" />
+          </View>
           <Text className="ml-4 font-bold text-xl text-mediumGreen w-5/6">{question}</Text>
         </View>
         {isOpen && (
