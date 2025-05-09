5dea9c9f9c0b1e2dea5355d4af28bc1043f2c2b9
Ryan Bui <buiswrld@gmail.com>
2025-05-08 18:51:13 -0700

    remove some duplicate button and opacity logic

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect, useRef } from "react";
-import { View, ImageBackground, Animated } from "react-native";
+import React, { useState } from "react";
+import { View, ImageBackground } from "react-native";
 import OnboardingSplash from "@/components/Onboarding/OnboardingSplash";
 import OnboardingQuiz from "@/components/Onboarding/OnboardingQuizFeature";
 import OnboardingScanner from "@/components/Onboarding/OnboardingScannerFeature";
 import OnboardingEnd from "@/components/Onboarding/OnboardingEnd";
-import NextButton from "@/components/Onboarding/NextButton";
 
 const onboardingPages = [
   OnboardingSplash,
@@ -15,43 +14,16 @@ const onboardingPages = [
 
 export default function Onboarding() {
   const [currentPage, setCurrentPage] = useState(0);
-  const [showNextButton, setShowNextButton] = useState(false);
-  const fadeAnim = useRef(new Animated.Value(0)).current; //set button opacity to 0 on start
 
   const CurrentPageComponent = onboardingPages[currentPage];
 
   const handleNext = () => {
     if (currentPage < onboardingPages.length - 1) {
       setCurrentPage((prev) => prev + 1);
-      setShowNextButton(false); // reset button visibility for the next page
-      fadeAnim.setValue(0); // reset opacity for the next page
     } else {
       console.log("Onboarding complete. Navigate to signup.");
     }
   };
-
-  useEffect(() => {
-    if (currentPage === 0) {
-      const timer = setTimeout(() => {
-        setShowNextButton(true);
-        //butoton fadein customization
-        Animated.timing(fadeAnim, {
-          toValue: 1,
-          duration: 1000,
-          useNativeDriver: true,
-        }).start();
-      }, 3500);
-
-      return () => clearTimeout(timer); 
-    } else {
-      setShowNextButton(true);
-      Animated.timing(fadeAnim, {
-        toValue: 1,
-        duration: 1000,
-        useNativeDriver: true,
-      }).start();
-    }
-  }, [currentPage]);
 
   return (
     <ImageBackground
@@ -60,19 +32,7 @@ export default function Onboarding() {
       resizeMode="cover"
     >
       <View style={{ flex: 1 }}>
-        <CurrentPageComponent />
-        {showNextButton && (
-          <Animated.View
-            style={{
-              opacity: fadeAnim,
-              position: "absolute",
-              bottom: 20,
-              right: 20,
-            }}
-          >
-            <NextButton onPress={handleNext} />
-          </Animated.View>
-        )}
+        <CurrentPageComponent onNext={handleNext} />
       </View>
     </ImageBackground>
   );
