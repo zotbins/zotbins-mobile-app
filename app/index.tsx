c9c78071a766bc28d3d45aabb4656b99e36fb28b
cyrusbuffington <cyrusbuffington@gmail.com>
2025-05-08 17:17:43 -0700

    remove useless imports

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,6 @@
-import { View, Text, Image, Pressable } from "react-native";
-import React, { useCallback, useEffect, useState } from "react";
-import Colors from "@/constants/Colors";
-import { Redirect, useRouter } from "expo-router";
-import ZotbinsLogo from "../assets/images/zotbins_logo.png";
+import { View, Image } from "react-native";
+import React from "react";
 import "../global.css";
-import { currentUser } from "./_layout";
 import SplashScreenImage from "../assets/images/splash-screen.png";
 
 const Index = () => {
