6582d5d53191ce7ac28ff53471743ec37b6530d9
cyrusbuffington <cyrusbuffington@gmail.com>
2025-05-08 23:44:00 -0700

    updateweeklymission functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,11 @@
 
 import * as admin from "firebase-admin";
 import {updateDailyMissions, testUpdateDailyMissions} from "./updateDailyMissions";
+import {updateWeeklyMissions, testUpdateWeeklyMissions} from "./updateWeeklyMissions";
 
 admin.initializeApp();
 
 exports.updateDailyMissions = updateDailyMissions;
+exports.updateWeeklyMissions = updateWeeklyMissions;
 exports.testUpdateDailyMissions = testUpdateDailyMissions;
+exports.testUpdateWeeklyMissions = testUpdateWeeklyMissions;
---
+++ functions/src/updateWeeklyMissions.ts
@@ -0,0 +1,45 @@
+import * as functions from 'firebase-functions/v1';
+import * as admin from 'firebase-admin';
+
+const updateWeeklyMissionsLogic = async () => {
+    const db = admin.firestore();
+    try {
+        // get all missions with type "weekly"
+        const missions = await db.collection("missions").where("type", "==", "weekly").get();
+
+        if (missions.empty) {
+            console.log("No weekly missions found.");
+            return;
+        }
+
+        const batch = db.batch();
+        // set all missions to have status false
+        missions.forEach((mission: any) => {
+            batch.update(mission.ref, { status: false });
+        });
+
+        // choose 3 random weekly missions to make active
+        const missionNames: string[] = [];
+        const randomMissions = missions.docs.sort(() => Math.random() - 0.5).slice(0, 3);
+        randomMissions.forEach((mission: any) => {
+            batch.update(mission.ref, { status: true });
+            missionNames.push(mission.data().title);
+        });
+        await batch.commit();
+
+        console.log("Weekly missions updated");
+        console.log("Active weekly missions: ", missionNames);
+    } catch (error) {
+        console.error("Error updating weekly missions: ", error);
+    }
+    return null;
+};
+
+export const updateWeeklyMissions = functions.pubsub.schedule('0 0 * * 1') // Runs every Monday at midnight UTC
+  .timeZone('UTC').onRun(updateWeeklyMissionsLogic);
+
+// HTTP function to test weekly missions update
+export const testUpdateWeeklyMissions = functions.https.onRequest(async (req, res) => {
+    await updateWeeklyMissionsLogic();
+    res.send("Weekly missions updated");
+});
