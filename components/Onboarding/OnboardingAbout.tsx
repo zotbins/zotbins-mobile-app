29adcc31b14cab3b32d94f2aabf0cd50ddf40e18
Ryan Bui <buiswrld@gmail.com>
2025-05-08 19:09:27 -0700

    move onboardingpage design to a customizable component for reproducability

--- components/Onboarding/OnboardingAbout.tsx
+++ components/Onboarding/OnboardingAbout.tsx
@@ -1,64 +1,15 @@
 import React from "react";
-import { View, Text } from "react-native";
-import NextButton from "@/components/Onboarding/NextButton";
+import OnboardingPage from "@/components/Onboarding/OnboardingPage";
 import RecycleIcon from "@/assets/images/onboarding/recycle_icon.svg";
 
 const OnboardingAbout = ({ onNext }: { onNext: () => void }) => {
   return (
-    <View style={{ flex: 1 }}>
-      {/* Centered Content */}
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          paddingHorizontal: 20,
-        }}
-      >
-        {/* Recycle Icon */}
-        <RecycleIcon width={200} height={200} />
-
-        {/* Header Text */}
-        <View style={{ marginTop: 20, alignSelf: "flex-start" }}>
-          <Text
-            style={{
-              fontSize: 36,
-              fontWeight: "bold",
-              color: "#00762B", // Dark green color
-              textAlign: "left",
-            }}
-          >
-            Welcome to ZotBins!
-          </Text>
-        </View>
-
-        {/* Description Text */}
-        <View style={{ marginTop: 10, alignSelf: "flex-start" }}>
-          <Text
-            style={{
-              fontSize: 18,
-              color: "#00762B",
-              textAlign: "left",
-              lineHeight: 24,
-            }}
-          >
-            An innovative smart waste bin system designed to optimize waste
-            management efficiency.
-          </Text>
-        </View>
-      </View>
-
-      {/* Bottom-right Next Button */}
-      <View
-        style={{
-          position: "absolute",
-          bottom: 40,
-          right: 20,
-        }}
-      >
-        <NextButton onPress={onNext} />
-      </View>
-    </View>
+    <OnboardingPage
+      Icon={RecycleIcon}
+      headerText="Welcome to ZotBins!"
+      bodyText="An innovative smart waste bin system designed to optimize waste management efficiency."
+      onNext={onNext}
+    />
   );
 };
 
---
+++ components/Onboarding/OnboardingPage.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { View, Text } from "react-native";
+import NextButton from "@/components/Onboarding/NextButton";
+
+interface OnboardingPageProps {
+  Icon: React.FC<{ width: number; height: number }>;
+  headerText: string;
+  bodyText: string;
+  onNext: () => void;
+}
+
+const OnboardingPage: React.FC<OnboardingPageProps> = ({
+  Icon,
+  headerText,
+  bodyText,
+  onNext,
+}) => {
+  return (
+    <View style={{ flex: 1 }}>
+      {/* Centered Content */}
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          paddingHorizontal: 20,
+        }}
+      >
+        {/* Icon */}
+        <Icon width={200} height={200} />
+
+        {/* Header Text */}
+        <View style={{ marginTop: 20, alignSelf: "flex-start" }}>
+          <Text
+            style={{
+              fontSize: 36,
+              fontWeight: "bold",
+              color: "#00762B",
+              textAlign: "left",
+            }}
+          >
+            {headerText}
+          </Text>
+        </View>
+
+        {/* Body Text */}
+        <View style={{ marginTop: 10, alignSelf: "flex-start" }}>
+          <Text
+            style={{
+              fontSize: 18,
+              color: "#00762B",
+              textAlign: "left",
+              lineHeight: 24,
+            }}
+          >
+            {bodyText}
+          </Text>
+        </View>
+      </View>
+
+      {/* Bottom-right Next Button */}
+      <View
+        style={{
+          position: "absolute",
+          bottom: 40,
+          right: 20,
+        }}
+      >
+        <NextButton onPress={onNext} />
+      </View>
+    </View>
+  );
+};
+
+export default OnboardingPage;
