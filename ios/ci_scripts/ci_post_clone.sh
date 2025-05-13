56a67b234c27b5d54a1043d7227488b07712e871
sophiabtran <transb1@uci.edu>
2025-05-12 18:59:00 -0700

    moved scripts

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-npm install 
+curl -L https://nodejs.org/dist/v18.17.1/node-v18.17.1-darwin-x64.tar.gz -o node.tar.gz
+mkdir -p /tmp/nodejs
+tar -xzf node.tar.gz -C /tmp/nodejs --strip-components=1
+export PATH="/tmp/nodejs/bin:$PATH"
 
-cd /Volumes/workspace/repository/ios
+npm install
 
 brew install cocoapods
 
